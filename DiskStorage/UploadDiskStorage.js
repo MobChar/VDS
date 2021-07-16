92ff79f50714da92028ffd65dffe2d570639abf5
MobChar <65935190+MobChar@users.noreply.github.com>
2021-07-16 16:34:48 +0700

    Update UploadDiskStorage.js

--- DiskStorage/UploadDiskStorage.js
+++ DiskStorage/UploadDiskStorage.js
@@ -13,7 +13,7 @@ var storage = multer.diskStorage({
         if (!fs.existsSync(Constants.VIDEO_ASSET_DIR)){
             fs.mkdirSync(Constants.VIDEO_ASSET_DIR);
         }
-
+     
         let ext = path.extname(file.originalname);
         if (ext === '.jpg')
             cb(null, Constants.IMAGE_ASSET_DIR);
@@ -31,9 +31,17 @@ var upload = multer({
     storage: storage,
     fileFilter: function (req, file, cb) {
         let ext = path.extname(file.originalname);
-        if (ext === '.jpg' ||  ext === '.mp4')
-            cb(null, true)
-        else cb(null, false)
+        if (ext === '.jpg' ){
+            if (file.size<=1024)
+             return cb(null, true);
+            else return cb(new Error('Image size <= 1024 bytes'));
+        }
+        else if(ext === '.mp4'){
+            if (file.size<=10240)
+            return cb(null, true);
+           else return cb(new Error('Video size <= 10240 bytes'));
+        }
+        else cb(new Error('File type not supported'));
 
         // // You can always pass an error if something goes wrong:
         // cb(new Error('File type not supported'))
